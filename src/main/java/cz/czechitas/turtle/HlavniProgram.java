b2cb346f3712d103a5a4af2116db979d31f69c58
LucyV25 <lucievoleman@gmail.com>
2023-02-21 22:20:48 +0100

    nakresleni snehulaka, zmrzliny a masinky

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,8 +9,50 @@ public class HlavniProgram {
 
     public void start() {
         zofka = new Turtle();
-        zofka.setLocation(100.0,100.0);
-        kolecko(3, Color.yellow);
+
+        //zmrzlina
+
+        zofka.setLocation(100.0,300.0);
+        zofka.turnLeft(30);
+        trojuhelnik(160, Color.black);
+        zofka.setLocation(35.0,190.0);
+        kolecko(7, Color.black);
+
+        //snehulak
+        zofka.setLocation(300.0,100.0);
+        kolecko(4, Color.blue);
+        zofka.setLocation(280.0,230.0);
+        kolecko(6, Color.blue);
+        zofka.setLocation(260.0,400.0);
+        kolecko(8, Color.blue);
+        zofka.setLocation(220.0,200.0);
+        kolecko(2, Color.blue);
+        zofka.setLocation(410.0,200.0);
+        kolecko(2, Color.blue);
+
+        //masinka
+        zofka.setLocation(800.0,400.0);
+        zofka.turnRight(30);
+        obdelnik(140,90, Color.red);
+        kolecko(4, Color.red);
+        zofka.turnLeft(90);
+        zofka.penUp();
+        zofka.move(130);
+        zofka.penDown();
+        zofka.turnRight(90);
+        obdelnik(70,130, Color.red);
+        zofka.turnLeft(45);
+        rovnoramennyTrojuhelnik(50, Color.red);
+        zofka.turnLeft(90);
+        zofka.penUp();
+        zofka.move(20);
+        zofka.penDown();
+        kolecko(2, Color.red);
+        zofka.penUp();
+        zofka.move(60);
+        zofka.penDown();
+        kolecko(2, Color.red);
+
     }
 
     public void ctverec(double velikostStrany, Color barva){
@@ -51,7 +93,7 @@ public class HlavniProgram {
 
     public void kolecko(double velikost, Color barva) {
         zofka.setPenColor(barva);
-        for (int i = 0; i < 75; i++) {
+        for (int i = 0; i < 72; i++) {
             zofka.move(velikost);
             zofka.turnRight(5);
 
